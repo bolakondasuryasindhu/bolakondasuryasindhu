125757d168d5921bfb0c1dd662d7bf4a829531be
bolakondasuryasindhu <168550731+bolakondasuryasindhu@users.noreply.github.com>
2024-05-09 17:04:17 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,22 @@
+public class Book {
+      private String book_Name,book_Author;
+      private double price;
+    
+       public void getBookInfo(String bn,String aut,double p)
+       {
+    	  book_Name=bn;
+    	  book_Author=aut;
+    	  price=p;
+       }
+       public void disBookDet()
+       {
+    	   System.out.println(book_Name+" "+book_Author+" "+price);
+       }
+	public static void main(String[] args) {
+	
+           Example8 ob=new Example8();
+           ob.getBookInfo("How to success", "xxxx",250);
+           ob.disBookDet();
+	}
+
+}
