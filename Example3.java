8fcf14c54c358ace4827ec4cf59052ee104773d8
bolakondasuryasindhu <168550731+bolakondasuryasindhu@users.noreply.github.com>
2024-05-09 16:47:07 +0530

    Add files via upload

---
+++ Example3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Example3{
+
+	public static void main(String[] args) {
+		char ch;
+		System.out.println("Enter a character:");
+		Scanner sc=new Scanner(System.in);
+		ch=sc.next().charAt(0);
+		int a=(int)ch;
+		System.out.println("ASCII of "+ch+" is: "+a);
+	}
+
+}
