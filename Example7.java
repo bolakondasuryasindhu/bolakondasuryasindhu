815fa9b33f57ea688ea15478e63d25781b4c039f
bolakondasuryasindhu <168550731+bolakondasuryasindhu@users.noreply.github.com>
2024-05-09 17:16:47 +0530

    Add files via upload

---
+++ Example7.java
@@ -0,0 +1,13 @@
+public class Example7{
+	static int x=5;
+	static {
+		System.out.println(x++);
+		x++;
+		System.out.println(x);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Main Block");
+	}
+
+}
