6bf719682714537afcf61aca6e532a4f8f83e502
bolakondasuryasindhu <168550731+bolakondasuryasindhu@users.noreply.github.com>
2024-05-09 17:20:08 +0530

    Add files via upload

---
+++ BookMain.java
@@ -0,0 +1,10 @@
+public class BookMain {
+
+	public static void main(String[] args) {
+		Book bm1 = new Book(101,"aaa");
+		Book bm2 = new Book(102,"bbb");
+		System.out.println(bm1);
+		System.out.println(bm2);
+	}
+
+}
