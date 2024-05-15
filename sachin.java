58faad66c850111aaa3e4782fcafc304c5371b42
bolakondasuryasindhu <168550731+bolakondasuryasindhu@users.noreply.github.com>
2024-05-15 23:00:15 +0530

    Add files via upload

---
+++ sachin.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class sachin {
+    public static void main(String[] args)
+    {
+        String name = "Sachin";
+        name = name.concat(" Tendulkar");
+        System.out.println(name);
+    }
+}
