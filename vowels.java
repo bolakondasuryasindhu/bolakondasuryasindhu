f64fe6138f89c5f49f44105bdcf88bb3c9cc10ac
bolakondasuryasindhu <168550731+bolakondasuryasindhu@users.noreply.github.com>
2024-05-15 13:32:46 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,21 @@
+class vowels{
+
+public static void main(String[]args)
+{
+int j=5;
+		System.out.println(j);
+		j++;
+		System.out.println(j);
+		String name="abc";
+		name=name.concat("def");
+		System.out.println(name);
+		System.out.println(name.concat("def"));
+ char[] ch= {'a','e','i','u'};
+ String s=new String(ch);
+ System.out.println(s);
+ for(int i=0;i<ch.length;i++)
+ {
+	 System.out.println(ch[i]);
+ }
+}
+}
