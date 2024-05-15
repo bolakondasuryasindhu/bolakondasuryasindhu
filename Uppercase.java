1ad33b07201437f28e4387b0e715e11e6f703cbe
bolakondasuryasindhu <168550731+bolakondasuryasindhu@users.noreply.github.com>
2024-05-15 23:21:38 +0530

    Add files via upload

---
+++ Uppercase.java
@@ -0,0 +1,16 @@
+class Uppercase {
+ 
+    
+    public static void main(String args[])
+    {
+        
+        String str = "Welcome! to Geeksforgeeks";
+ 
+        
+      
+        String strup = str.toUpperCase();
+ 
+        
+        System.out.println(strup);
+    }
+}
