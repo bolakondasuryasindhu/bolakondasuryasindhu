ad93e2aba02214e3ed6684d04c2a0c4f6c2898ae
bolakondasuryasindhu <168550731+bolakondasuryasindhu@users.noreply.github.com>
2024-06-04 17:26:47 +0530

    Add files via upload

---
+++ Boxing.java
@@ -0,0 +1,13 @@
+class Boxing
+ {
+  public static void main(String[]args)
+	 {
+  int a=40;
+  Integer b1 = new Integer(a); // autoboxing
+
+  Integer b2=90;
+
+  System.out.println(b1+""+b2);
+
+}
+}
